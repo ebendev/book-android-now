efe560cdb818f7568beaa9f8672bb6506041ae83
Eben Geer <me@eben.co>
2012-01-05 12:06:55 -0600

    initial hello world from Part I

---
+++ src/com/thirteen23/android/now/Now.java
@@ -0,0 +1,13 @@
+package com.thirteen23.android.now;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Now extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
