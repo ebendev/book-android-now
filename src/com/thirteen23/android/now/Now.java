f97be8989e0f8ba0bac845f4d4db9c4034bb5fae
Eben Geer <me@eben.co>
2012-01-05 14:49:12 -0600

    button updates time, ch 7

--- src/com/thirteen23/android/now/Now.java
+++ src/com/thirteen23/android/now/Now.java
@@ -2,12 +2,32 @@ package com.thirteen23.android.now;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Now extends Activity {
+import java.util.Date;
+
+public class Now extends Activity implements View.OnClickListener {
+    Button button;
+    
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        button = new Button(this);
+        button.setOnClickListener(this);
+
+        updateTime();
+
+        setContentView(button);
+    }
+
+    public void onClick(View view) {
+        updateTime();
+    }
+
+    private void updateTime() {
+        button.setText(new Date().toString());
     }
 }
